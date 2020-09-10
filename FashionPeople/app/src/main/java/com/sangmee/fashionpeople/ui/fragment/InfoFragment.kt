4cb6a4684e25461ce8d9f9130974fccd7bb2a9f8
sangminkim <apfhdznzl@gmail.com>
2020-09-10 16:27:09 +0900

    #10 change codestyle and remove unnecessary code in InfoFragment

--- FashionPeople/app/src/main/java/com/sangmee/fashionpeople/ui/fragment/InfoFragment.kt
+++ FashionPeople/app/src/main/java/com/sangmee/fashionpeople/ui/fragment/InfoFragment.kt
@@ -5,21 +5,13 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
-import com.bumptech.glide.Glide
-import com.sangmee.fashionpeople.ui.FeedImageAdapter
 import com.sangmee.fashionpeople.R
 import com.sangmee.fashionpeople.kakaologin.GlobalApplication
-import com.sangmee.fashionpeople.retrofit.model.FUser
 import com.sangmee.fashionpeople.retrofit.RetrofitClient
 import com.sangmee.fashionpeople.retrofit.model.FeedImage
-import de.hdodenhof.circleimageview.CircleImageView
+import com.sangmee.fashionpeople.ui.FeedImageAdapter
 import kotlinx.android.synthetic.main.fragment_info.*
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -28,89 +20,57 @@ import retrofit2.Response
 class InfoFragment : Fragment() {
 
     lateinit var customId: String
-    var customName: String? = null
-    var profileImgName: String? = null
     private val feedImageAdapter by lazy {
         FeedImageAdapter(customId)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        customId = GlobalApplication.prefs.getString("custom_id", "???")
-        //retrofit으로 회원 닉네임 알아내기
-
-
+        customId = GlobalApplication.prefs.getString("custom_id", "")
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
 
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_info, container, false)
-        //툴바 세팅
-        runBlocking {
-            val a = launch {
-                RetrofitClient().getFUserService().getFUser(customId).enqueue(object: Callback<FUser> {
-                    override fun onFailure(call: retrofit2.Call<FUser>, t: Throwable) {
-                        Log.d("sangmin_error", t.message)
-                    }
-
-                    override fun onResponse(call: retrofit2.Call<FUser>, response: Response<FUser>) {
-                        //닉네임 레트로핏으로 불러오기
-                        customName = response.body()?.name.toString()
-                        profileImgName = response.body()?.profileImage.toString()
-                        //툴바 세팅
-                        val toolbar = view.findViewById<Toolbar>(R.id.app_toolbar)
-                        if(activity is AppCompatActivity){
-                            (activity as AppCompatActivity).setSupportActionBar(toolbar)
-                            val actionBar = (activity as AppCompatActivity).supportActionBar!!
-                            val toolbar_title = view.findViewById<TextView>(R.id.toolbar_title)
-                            toolbar_title.setText(customName)
-                            actionBar.setDisplayShowTitleEnabled(false)
-                            actionBar.setDisplayHomeAsUpEnabled(true)
-                        }
-                        val profileImg = view.findViewById<CircleImageView>(R.id.profile_image)
-
-                        Glide.with(context!!).load("https://fashionprofile-images.s3.ap-northeast-2.amazonaws.com/users/${customId}/profile/${profileImgName}").error(R.drawable.user).into(profileImg)
-
-                    }
-                })
-            }
-            a.join()
-        }
-
-
-
-        return view
+        return inflater.inflate(R.layout.fragment_info, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
+        //리사이클러뷰에 adpater 세팅 후 레트로핏으로 저장한 이미지 적용
         rv_feed_image.apply {
             adapter = feedImageAdapter
         }
         getFeedImages()
     }
+
     private fun getFeedImages() {
-        RetrofitClient().getFeedImageService().getFeedImages(customId).enqueue(object: Callback<List<FeedImage>> {
-            override fun onResponse(
-                call: Call<List<FeedImage>>,
-                response: Response<List<FeedImage>>
-            ) {
-                response.body()?.let { feedImages ->
-                    feedImageAdapter.setFeedImages(feedImages)
-                }
-                Log.d("feedUrls", response.body()!!.size.toString())
-                response.body()?.forEach{
+        if (customId != "") {
+            RetrofitClient().getFeedImageService().getFeedImages(customId)
+                .enqueue(object : Callback<List<FeedImage>> {
+                    override fun onResponse(
+                        call: Call<List<FeedImage>>,
+                        response: Response<List<FeedImage>>
+                    ) {
+                        response.body()?.let { feedImages ->
+                            feedImageAdapter.setFeedImages(feedImages)
+                        }
+                        Log.d("feedUrls", response.body()!!.size.toString())
+                        response.body()?.forEach {
 
-                }
+                        }
 
-            }
+                    }
 
-            override fun onFailure(call: Call<List<FeedImage>>, t: Throwable) {
-                Log.d("fail", t.message)
-            }
-        })
+                    override fun onFailure(call: Call<List<FeedImage>>, t: Throwable) {
+                        Log.d("fail", t.message)
+                    }
+                })
+        }
     }
-
 }
