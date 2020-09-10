8a577e1618890cb8c3f86df8aa7fc04593eff34f
sangminkim <apfhdznzl@gmail.com>
2020-09-10 21:53:49 +0900

    #10 set imageview at gallery

---
+++ FashionPeople/app/src/main/java/com/sangmee/fashionpeople/s3/RemoteDataSourceImpl.kt
@@ -0,0 +1,70 @@
+package com.sangmee.fashionpeople.s3
+
+import android.content.Context
+import android.util.Log
+import com.amazonaws.auth.CognitoCachingCredentialsProvider
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferListener
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferNetworkLossHandler
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferState
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility
+import com.amazonaws.regions.Region
+import com.amazonaws.regions.Regions
+import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.services.s3.model.CannedAccessControlList
+import java.io.File
+
+class RemoteDataSourceImpl(private val context: Context, private val customId: String) :
+    RemoteDataSource {
+
+    //aws s3에 이미지 업로드
+    override fun uploadWithTransferUtility(fileName: String, file: File?) {
+
+        val credentialsProvider = CognitoCachingCredentialsProvider(
+            context,
+            "ap-northeast-2:04a21c16-627a-49a9-8229-f1c412ddebfa",  // 자격 증명 풀 ID
+            Regions.AP_NORTHEAST_2 // 리전
+        )
+
+        TransferNetworkLossHandler.getInstance(context)
+
+        val transferUtility = TransferUtility.builder()
+            .context(context)
+            .defaultBucket("fashionprofile-images")
+            .s3Client(AmazonS3Client(credentialsProvider, Region.getRegion(Regions.AP_NORTHEAST_2)))
+            .build()
+
+        /* Store the new created Image file path */
+
+        val uploadObserver = transferUtility.upload(
+            "users/${customId}/profile/${fileName}",
+            file,
+            CannedAccessControlList.PublicRead
+        )
+
+        //CannedAccessControlList.PublicRead 읽기 권한 추가
+
+        // Attach a listener to the observer
+        uploadObserver.setTransferListener(object : TransferListener {
+            override fun onStateChanged(id: Int, state: TransferState) {
+                if (state == TransferState.COMPLETED) {
+                    Log.d("MYTAG", "fileupload")
+                }
+            }
+
+            override fun onProgressChanged(id: Int, current: Long, total: Long) {
+                val done = (((current.toDouble() / total) * 100.0).toInt())
+                Log.d("MYTAG", "UPLOAD - - ID: $id, percent done = $done")
+            }
+
+            override fun onError(id: Int, ex: Exception) {
+                Log.d("MYTAG", "UPLOAD ERROR - - ID: $id - - EX: ${ex.message.toString()}")
+            }
+        })
+
+        // If you prefer to long-poll for updates
+        if (uploadObserver.state == TransferState.COMPLETED) {
+            /* Handle completion */
+
+        }
+    }
+}
--- FashionPeople/app/src/main/java/com/sangmee/fashionpeople/ui/fragment/InfoFragment.kt
+++ FashionPeople/app/src/main/java/com/sangmee/fashionpeople/ui/fragment/InfoFragment.kt
@@ -1,25 +1,37 @@
 package com.sangmee.fashionpeople.ui.fragment
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.database.Cursor
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.sangmee.fashionpeople.R
 import com.sangmee.fashionpeople.kakaologin.GlobalApplication
 import com.sangmee.fashionpeople.retrofit.RetrofitClient
 import com.sangmee.fashionpeople.retrofit.model.FeedImage
 import com.sangmee.fashionpeople.ui.FeedImageAdapter
+import com.theartofdev.edmodo.cropper.CropImage
 import kotlinx.android.synthetic.main.fragment_info.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.io.File
 
 
 class InfoFragment : Fragment() {
 
     lateinit var customId: String
+    var file: File? = null
     private val feedImageAdapter by lazy {
         FeedImageAdapter(customId)
     }
@@ -41,6 +53,40 @@ class InfoFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        //사진 등록 imageView 클릭시 이벤트
+        profile_image.setOnClickListener {
+            if (context?.let {
+                    ContextCompat.checkSelfPermission(
+                        it,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    )
+                } == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+                    context!!,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                try {
+                    //갤러리 앱 실행
+                    val intent = Intent(Intent.ACTION_PICK)
+                    intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*")
+                    startActivityForResult(intent, CHOOSE_PROFILEIMG)
+
+                } catch (e: java.lang.Exception) {
+                    e.printStackTrace()
+                }
+            } else {
+                activity?.let {
+                    ActivityCompat.requestPermissions(
+                        it,
+                        arrayOf(
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                            Manifest.permission.READ_EXTERNAL_STORAGE
+                        ),
+                        100
+                    )
+                }
+            }
+        }
 
         //리사이클러뷰에 adpater 세팅 후 레트로핏으로 저장한 이미지 적용
         rv_feed_image.apply {
@@ -72,5 +118,43 @@ class InfoFragment : Fragment() {
                     }
                 })
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == CHOOSE_PROFILEIMG && resultCode == AppCompatActivity.RESULT_OK && data != null) {
+            try {
+                val uri: Uri = data.data!!
+                CropImage.activity(uri)
+                    .setAspectRatio(1, 1)
+                    .start(context!!, this)
+            } catch (e: Exception) {
+
+            }
+        }
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            val result = CropImage.getActivityResult(data)
+            if (resultCode == AppCompatActivity.RESULT_OK) {
+                val resultUri = result.uri
+                profile_image.setImageURI(resultUri)
+
+
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Log.e("TAG_ERROR", result.error.toString())
+            }
+        }
+    }
+
+    //이미지Uri -- > 절대경로로 바꿔서 리턴시켜주는 메소드
+    private fun getRealPathFromUri(uri: Uri): String {
+        val proj: Array<String> = arrayOf(MediaStore.Images.Media.DATA)
+        val c: Cursor = context!!.contentResolver.query(uri, proj, null, null, null)!!
+        val index = c.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+        c.moveToFirst()
+        return c.getString(index)
+    }
+
+    companion object {
+        private const val CHOOSE_PROFILEIMG = 200
     }
 }
