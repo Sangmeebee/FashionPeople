5824fff0197dcd4343aa5c34d2d03fb6a3ff9366
seunghwan <slflfl12@naver.com>
2020-09-14 19:26:50 +0900

    #11 FeedImageDetailActivity 생성(사진까지만)

--- FashionPeople/app/src/main/java/com/sangmee/fashionpeople/ui/FeedImageDetailActivity.kt
+++ FashionPeople/app/src/main/java/com/sangmee/fashionpeople/ui/FeedImageDetailActivity.kt
@@ -2,14 +2,34 @@ package com.sangmee.fashionpeople.ui
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.sangmee.fashionpeople.R
+import com.sangmee.fashionpeople.kakaologin.GlobalApplication
+import com.sangmee.fashionpeople.retrofit.model.FeedImage
+import kotlinx.android.synthetic.main.activity_feed_image_detail.*
 
-class FeedImageDetailActivity: AppCompatActivity() {
+class FeedImageDetailActivity : AppCompatActivity() {
 
+    private lateinit var customId: String
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_feed_image_detail)
+
+        customId = GlobalApplication.prefs.getString("custom_id", "")
+
+        intent?.getParcelableExtra<FeedImage>(KEY_FEED_IMAGE)?.let { feedImage ->
+            Glide.with(this)
+                .load("https://fashionprofile-images.s3.ap-northeast-2.amazonaws.com/users/$customId/feed/${feedImage.imageName}")
+                .into(iv_feed_image)
+
+        }
+
+    }
+
+    companion object {
+        const val KEY_FEED_IMAGE = "key_feed_image"
     }
 }
