439ff06126a4459b9e0fe129121a89f325172386
seunghwan <slflfl12@naver.com>
2020-10-31 22:42:29 +0900

    #15 FeedImage 평가하기 메소드 추가

--- FashionPeople/app/src/main/java/com/sangmee/fashionpeople/retrofit/service/FeedImageService.kt
+++ FashionPeople/app/src/main/java/com/sangmee/fashionpeople/retrofit/service/FeedImageService.kt
@@ -1,12 +1,10 @@
 package com.sangmee.fashionpeople.retrofit.service
 
+import com.sangmee.fashionpeople.retrofit.model.Evaluation
 import com.sangmee.fashionpeople.retrofit.model.FeedImage
 import io.reactivex.Single
 import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.POST
-import retrofit2.http.Path
+import retrofit2.http.*
 
 interface FeedImageService {
 
@@ -15,6 +13,9 @@ interface FeedImageService {
 
     @GET("feedImage/other/{id}")
     fun getOtherImages(@Path("id") id: String): Single<List<FeedImage>>
+
+    @PUT("feedImage/evaluation/{imageId}")
+    fun updateImageScore(@Path("imageId") imageId: Int, evaluation: Evaluation)
 
     @GET("feedImage/{id}")
     fun getFeedImages(@Path("id") id: String): Call<List<FeedImage>>
