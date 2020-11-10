5eb23462c091ca380e2f30a04a50e3cdf3ba3de2
seunghwan <slflfl12@naver.com>
2020-11-10 15:29:50 +0900

    merge 에러 수정

--- FashionPeople/app/src/main/java/com/sangmee/fashionpeople/ui/fragment/comment/CommentDialogFragment.kt
+++ FashionPeople/app/src/main/java/com/sangmee/fashionpeople/ui/fragment/comment/CommentDialogFragment.kt
@@ -8,13 +8,25 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.library.baseAdapters.BR
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.sangmee.fashionpeople.R
 import com.sangmee.fashionpeople.databinding.FragmentCommentBinding
+import com.sangmee.fashionpeople.ui.fragment.home.evaluate.EvaluateViewModel
 
 class CommentDialogFragment : BottomSheetDialogFragment() {
 
     private lateinit var binding: FragmentCommentBinding
+
+    private val viewModel: CommentViewModel by lazy {
+        ViewModelProvider(this, object : ViewModelProvider.Factory {
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                return CommentViewModel() as T
+            }
+        }).get(CommentViewModel::class.java)
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,7 +34,7 @@ class CommentDialogFragment : BottomSheetDialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_comment, container, false)
-        binding.setVariable(BR.vm, vm)
+        binding.vm = viewModel
         binding.lifecycleOwner = this
         dialog?.let {
             it.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
