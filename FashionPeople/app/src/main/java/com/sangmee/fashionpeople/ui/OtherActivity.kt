57fc4e849d5b8dcd83cb6952c1973f198b0e0024
sangminkim <apfhdznzl@gmail.com>
2020-11-11 20:20:33 +0900

    #14 Create OtherActivity

---
+++ FashionPeople/app/src/main/java/com/sangmee/fashionpeople/ui/OtherActivity.kt
@@ -0,0 +1,12 @@
+package com.sangmee.fashionpeople.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sangmee.fashionpeople.R
+
+class OtherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_other)
+    }
+}
