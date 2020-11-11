8a6e0eed325438570ea9a07bbfd276912cb24c94
sangminkim <apfhdznzl@gmail.com>
2020-11-11 22:56:50 +0900

    #14 FeedImageFragment dataBinding 제거

--- FashionPeople/app/src/main/java/com/sangmee/fashionpeople/ui/fragment/info/content/FeedImageFragment.kt
+++ FashionPeople/app/src/main/java/com/sangmee/fashionpeople/ui/fragment/info/content/FeedImageFragment.kt
@@ -4,50 +4,37 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.RecyclerView
 import com.sangmee.fashionpeople.R
-import com.sangmee.fashionpeople.databinding.FragmentFeedImageBinding
 import com.sangmee.fashionpeople.observer.FeedImageViewModel
 import kotlinx.android.synthetic.main.fragment_feed_image.*
 
 class FeedImageFragment : Fragment() {
 
     private val vm by activityViewModels<FeedImageViewModel>()
-    private lateinit var binding: FragmentFeedImageBinding
     private val feedImageAdapter by lazy {
         FeedImageAdapter(vm.customId)
     }
-
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_feed_image, container, false)?.apply {
-            binding = DataBindingUtil.bind(this)!!
-            binding.lifecycleOwner = viewLifecycleOwner
-            vm.callFeedImages()
-            setRecyclerView()
-            viewModelCallback()
-        }
+        return inflater.inflate(R.layout.fragment_feed_image, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        rv_user_image.apply {
-            adapter = feedImageAdapter
-        }
+        vm.callFeedImages()
+        viewModelCallback()
+        setRecyclerView()
     }
 
     private fun setRecyclerView() {
-        binding.rvUserImage.apply {
+        rv_user_image.apply {
             setHasFixedSize(true)
             adapter = feedImageAdapter
         }
