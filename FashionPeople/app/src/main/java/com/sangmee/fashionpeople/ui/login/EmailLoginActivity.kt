8090d9a36a3629f58f82ef7ef08faa71ab670845
sangminkim <apfhdznzl@gmail.com>
2020-12-26 21:05:44 +0900

    #22 이메일 로그인 완료

--- FashionPeople/app/src/main/java/com/sangmee/fashionpeople/ui/login/EmailLoginActivity.kt
+++ FashionPeople/app/src/main/java/com/sangmee/fashionpeople/ui/login/EmailLoginActivity.kt
@@ -11,6 +11,8 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.sangmee.fashionpeople.R
 import com.sangmee.fashionpeople.data.GlobalApplication
+import com.sangmee.fashionpeople.data.dataSource.remote.FUserRemoteDataSourceImpl
+import com.sangmee.fashionpeople.data.repository.FUserRepositoryImpl
 import com.sangmee.fashionpeople.databinding.ActivityEmailLoginBinding
 import com.sangmee.fashionpeople.ui.MainActivity
 
@@ -18,6 +20,9 @@ class EmailLoginActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityEmailLoginBinding
     private lateinit var auth: FirebaseAuth
+    private val fUserRepository by lazy {
+        FUserRepositoryImpl(FUserRemoteDataSourceImpl())
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,26 +34,34 @@ class EmailLoginActivity : AppCompatActivity() {
     }
 
     fun clickLoginBtn() {
-        //Todo retrofit에서 아이디체크로 바꾸기
-        auth.signInWithEmailAndPassword(
-            binding.etEmail.text.toString(),
-            binding.etPassword.text.toString()
-        )
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    GlobalApplication.prefs.setString("email_custom_id", binding.etEmail.text.toString())
-                    Intent(this, MainActivity::class.java)
-                    startActivity(intent)
-                    finish()
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w("Sangmeebee", "signInWithEmail:failure", task.exception)
-                    Toast.makeText(
-                        baseContext, task.exception?.message.toString(),
-                        Toast.LENGTH_SHORT
-                    ).show()
+
+        var idExist = false
+        var passwordExist = false
+        fUserRepository.getAllFUser(success = {
+            for (fUser in it) {
+                if (fUser.id == binding.etEmail.text.toString()) {
+                    idExist = true
+                    if (fUser.password == binding.etPassword.text.toString()) {
+                        passwordExist = true
+                    }
                 }
             }
+            if (!idExist) {
+                Toast.makeText(this, "해당하는 아이디가 없습니다", Toast.LENGTH_SHORT).show()
+            } else {
+                if (!passwordExist) {
+                    Toast.makeText(this, "비밀번호가 일치하지 않습니다", Toast.LENGTH_SHORT).show()
+                } else {
+                    GlobalApplication.prefs.setString(
+                        "email_custom_id",
+                        binding.etEmail.text.toString()
+                    )
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }
+        }, failed = { Log.e("fashionPeopleError", it) })
     }
 
     fun clickSignUpBtn() {
